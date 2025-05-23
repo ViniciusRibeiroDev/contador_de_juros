9ebd4bc21e996c80c35df593636eff75d35e33f4
ViniciusRibeiroDev <114515008+ViniciusRibeiroDev@users.noreply.github.com>
2025-05-23 19:40:31 +0000

    Correção de Juros

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [venda, setVenda] = useState(0)
+  const [venda, setVenda] = useState(0.0)
   const [totalComJuros, setTotalComJuros] = useState()
   const [configuracao, setConfiguracao] = useState(false)
 
   const setVendaFuncao = (valor) => {
-    setVenda(+valor)
+    valor ? setVenda(parseFloat(valor)) : setVenda(0.0)
   }
 
   const calcular = () => {
@@ -20,14 +20,14 @@ function App() {
 
   return (
     <>
-      <GrConfigure onClick={() => setConfiguracao(true)}/>
+      <GrConfigure onClick={() => setConfiguracao(true)} />
       <h1>Calculadora de Parcelas</h1>
       <form>
-        <input type="text" id="venda" onChange={(e) => setVendaFuncao(e.target.value)} placeholder='Valor da Venda' />
+        <input type="number" id="venda" onChange={(e) => setVendaFuncao(e.target.value)} placeholder='Valor da Venda' />
         <button type="button" onClick={() => { calcular() }}>Calcular</button>
       </form>
       {totalComJuros != undefined ? <RenderizadorDeJuros totalComJuros={totalComJuros} venda={venda} /> : <></>}
-      {configuracao ? <Configuracao setConfiguracao={setConfiguracao}/>: <></>}
+      {configuracao ? <Configuracao setConfiguracao={setConfiguracao} /> : <></>}
     </>
   )
 }
--- src/Renderizador/index.jsx
+++ src/Renderizador/index.jsx
@@ -1,11 +1,11 @@
 const RenderizadorDeJuros = ({ totalComJuros, venda }) => {
     return <ul>
         {totalComJuros.map((elt, ind) => {
-            return <li key={ind}>{ind + 1}x {(Number(elt) / (ind + 1)).toLocaleString('pt-br',
+            return <li key={ind}>{ind + 1}x {(parseFloat(elt) / (ind + 1)).toLocaleString('pt-br',
                 {
                     style: 'currency',
                     currency: 'BRL'
-                })} - {Number(elt).toLocaleString('pt-br',
+                })} - {parseFloat(elt).toLocaleString('pt-br',
                     {
                         style: 'currency',
                         currency: 'BRL'
