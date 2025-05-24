4a7e5378aaa41b1b9a7af6e9b0239f5905962c96
ViniciusRibeiroDev <114515008+ViniciusRibeiroDev@users.noreply.github.com>
2025-05-24 18:12:27 +0000

    Correção no Calculo dos Juros

--- src/Juros/calculadorDeJuros.js
+++ src/Juros/calculadorDeJuros.js
@@ -4,7 +4,9 @@ const calculadorDeJuros = (valorDaVenda) => {
     const valorComJuros = []
 
     for (let i = 0; i < 12; i++) {
-        valorComJuros[i] = Number(valorDaVenda + ((valorDaVenda * (parcelas[i] + 1.65)) / 100)).toFixed(2)
+        // valorComJuros[i] = Number(valorDaVenda + ((valorDaVenda * (parcelas[i] + 1.65)) / 100)).toFixed(2)
+
+        valorComJuros[i] = Number((valorDaVenda / (1 - (parcelas[i] / 100))).toFixed(2))
     }
 
     if (valorDaVenda >= 300) {
--- src/Juros/juros.js
+++ src/Juros/juros.js
@@ -3,10 +3,10 @@ const parcelas = {
     1: 4.16, // parcela_2x
     2: 4.71, // parcela_3x
     3: 5.26, // parcela_4x
-    4: 5.81, // parcela_5x
-    5: 6.36, // parcela_6x
-    6: 6.92, // parcela_7x
-    7: 7.47, // parcela_8x
+    4: 8.02, // parcela_5x
+    5: 8.02, // parcela_6x
+    6: 8.02, // parcela_7x
+    7: 8.02, // parcela_8x
     8: 8.02, // parcela_9x
     9: 8.57, // parcela_10x
     10: 9.12, // parcela_11x
