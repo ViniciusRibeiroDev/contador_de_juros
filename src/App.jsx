eceda527d0561d0e74e73eef42f8b54c6e8b957d
ViniciusRibeiroDev <114515008+ViniciusRibeiroDev@users.noreply.github.com>
2025-08-29 21:38:05 +0000

    Correção de Pequenos Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,9 @@ function App() {
     <>
       <GrConfigure onClick={() => setConfiguracao(true)} />
       <h1>Calculadora de Parcelas</h1>
-      <form>
-        <input type="number" id="venda" onChange={(e) => setVendaFuncao(e.target.value)} placeholder='Valor da Venda' />
-        <button type="button" onClick={() => { calcular() }}>Calcular</button>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="number" step="0.01" id="venda" onChange={(e) => setVendaFuncao(e.target.value)} placeholder='Valor da Venda' />
+        <button type="submit" onClick={() => { calcular() }}>Calcular</button>
       </form>
       {totalComJuros != undefined ? <RenderizadorDeJuros totalComJuros={totalComJuros} venda={venda} /> : <></>}
       {configuracao ? <Configuracao setConfiguracao={setConfiguracao} /> : <></>}
